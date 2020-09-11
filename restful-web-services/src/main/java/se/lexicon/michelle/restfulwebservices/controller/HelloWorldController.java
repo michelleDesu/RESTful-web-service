2aa393d9fa844063a13f7c23dbaa12e349b777ab
michelle <michelle.johansson85@gmail.com>
2020-09-11 09:51:40 +0200

    * added an example when using a path-variable

--- restful-web-services/src/main/java/se/lexicon/michelle/restfulwebservices/controller/HelloWorldController.java
+++ restful-web-services/src/main/java/se/lexicon/michelle/restfulwebservices/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package se.lexicon.michelle.restfulwebservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import se.lexicon.michelle.restfulwebservices.entity.HelloWorldBean;
 
@@ -17,5 +18,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldsBean(){
         return new HelloWorldBean("Hello world");
+    }
+
+    ///hello-world/path-variable/in28minutes
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World %s", name));
     }
 }
--- restful-web-services/src/main/java/se/lexicon/michelle/restfulwebservices/entity/HelloWorldBean.java
+++ restful-web-services/src/main/java/se/lexicon/michelle/restfulwebservices/entity/HelloWorldBean.java
@@ -39,4 +39,5 @@ public class HelloWorldBean {
         return "helloWorldBean{" + "message='" + message + '\'' +
                 '}';
     }
+
 }
